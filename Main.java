f1f0c78916817009903d2f9c9d1beea72d67f4d9
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-03-13 11:07:49 -0700

    finished hw

--- Main.java
+++ Main.java
@@ -1,36 +1,59 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    // TODO: Make an array of doubles, and test your sumArray method
+    double[] decimals = {3.3, 4.4, 4.64, 6.66, 2.42, 9.58, 2.34};
+    String[] words = {"dkafjkds", "fuoaieru", "eqiwruioqwru", "wieqruiosdfnkasdf", "eirqurojaskdfndasktjre", "oreeqwirojdsgiatjo", "iowuerpoiqweroindg", "qwoerigdvfdngoautirue", "aerioqweriajfdndgoafdjoadsiofj", "eroqewprieoprqjkknagklagdjf"};
+    int[] numbers = {6, 45, 134, 767, 8, 3, 6, 3, 3454, 7867, 234, 686, 343, 757, 23, 867, 234, 8686, 435, 234, 656, 4234, 6564, 2342, 757, 2323, 3454, 2343, 23423, 4575, 34234, 242};
 
-    // TODO: Make an array of Strings, and test your averageLength method
+    System.out.println(sumArray(decimals));
+    System.out.println(averageLength(words));
+    System.out.println(arrayMax(numbers));
+    System.out.println(linearSearch(words, "oreeqwirojdsgiatjo"));
+    System.out.println(linearSearch(words, "adsfk45934faksdfjkasjf"));
 
-    // TODO: Make an array of ints, and test your arrayMax method
-
-    // TODO: Make an array of Strings as well as a target, and test your linearSearch method
   }
 
 
   public static double sumArray(double[] arr)
   {
-    return 0.0;
+    int sum = 0;
+    for (double i : arr) {
+      sum += i;
+    }
+    return sum;
   }
 
   public static double averageLength(String[] arr)
   {
-    return 0.0;
+    double average = 0;
+    for (int i = 0; i < arr.length; i++) {
+      average += arr[i].length();
+    }
+    return average / arr.length;
   }
 
   public static int arrayMax(int[] arr)
   {
-    return Integer.MIN_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int i : arr) {
+      if (i > max) {
+        max = i;
+      }
+    }
+    return max;
   }
 
-  public static boolean linearSearch(String[] arr)
+  public static boolean linearSearch(String[] arr, String target)
   {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i].equals(target)) {
+        return true;
+      }
+    }
     return false;
   }
 }
